4c499307caad47464b287f2fb41d02f760707073
Declan <franklindudes21@gmail.com>
2023-02-20 11:00:06 +1100

    added image to site

--- template.php
+++ template.php
@@ -7,9 +7,9 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-sm bg-secondary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Navbar</a>
+        <a class="navbar-brand" href="index.php"><img src="./images/maccas-logo.jpg" width="200" height="100"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
